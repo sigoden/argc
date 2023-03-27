a12e1cbfa4ac2f706dff35af5f61f66d72d4ac4c
sigoden <sigoden@gmail.com>
2023-03-27 08:07:41 +0800

    refactor: improve compgen (#72)

--- completions/argc.fish
+++ completions/argc.fish
@@ -16,43 +16,27 @@ function __fish_complete_argc
     set -l IFS '\n'
     set -l compgen_values (argc --compgen "$scriptfile" "$line" 2>/dev/null)
     set -l candicates
-    set -l option_values
-    set -l value_kind 0
+    set -l arg_value
     for item in $compgen_values
         if string match -qr -- '^-' "$item"
-            set -a option_values $item
+            set -a candicates "$item"
         else if string match -qr '^`[^` ]+`' -- "$item"
             set -l name (string sub "$item" -s 2 -e -1)
             set -a candicates ("$ARGC_BASH" "$scriptfile" $name "$line" 2>/dev/null)
-        else if string match -q -- '<*' "$item"
-            if string match -qi -- '<args>...' "$item"
-                set value_kind 1
-            else if string match -qir -- '(file|path)' "$item"
-                set value_kind 2
-            else if string match -qir -- 'dir' "$item"
-                set value_kind 3
-            else
-                set value_kind 9
-            end
+        else if string match -q -- '<*' "$item" || string match -q -- '[*'
+            set arg_value "$item"
         else
-            set -a candicates $item
+            set -a candicates "$item"
         end
     end
-    if [ $value_kind -eq 0 ]
-        if test -z "$candicates"
-            set -a candicates $option_values
-        end
-    else if [ $value_kind -eq 1 ]
-        if test -z "$candicates"
-            set -a candicates $option_values
-        end
-        if test -z "$candicates"
+    if test -z "$candicates"
+        if string match -qir -- '(file|path)' "$item"
             __fish_complete_path
+        else if string match -qir -- 'dir' "$item"
+            __fish_complete_directories 
         end
-    else if [ $value_kind -eq 2 ]
-        __fish_complete_path
-    else if [ $value_kind -eq 3 ]
-        __fish_complete_directories 
+    else if test -n "$arg_value"
+        set -a candicates "$arg_value"
     end
     for item in $candicates
         echo $item
