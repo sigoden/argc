1f74053405a648d125251dc8e086304cf9f7eff8
sigoden <sigoden@gmail.com>
2023-04-03 19:40:36 +0800

    feat: improve bash compgen (#89)
    * feat: improve bash compgen
    
    * update tests
    
    * fix test

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,8 @@
 use assert_cmd::prelude::*;
 use std::process::Command;
 
+use crate::fixtures::{get_path_env_var, get_spec};
+
 #[test]
 fn version() {
     Command::cargo_bin("argc")
@@ -22,4 +24,20 @@ fn help() {
         .assert()
         .stderr(predicates::str::contains(env!("CARGO_PKG_DESCRIPTION")))
         .failure();
+}
+
+#[test]
+fn compgen() {
+    let (path, _) = get_spec();
+    let path_env_var = get_path_env_var();
+    Command::cargo_bin("argc")
+        .unwrap()
+        .arg("--argc-compgen")
+        .arg("bash")
+        .arg(path)
+        .arg("cmd_option_names --op11 ")
+        .env("PATH", path_env_var)
+        .assert()
+        .stdout(predicates::str::contains("a1\na2\na3"))
+        .success();
 }
