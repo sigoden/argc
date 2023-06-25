2fca1892118534f447841a2d56e2681e00a5a88d
sigoden <sigoden@gmail.com>
2023-06-26 07:37:13 +0800

    fix bash

--- src/bin/argc/completions/argc.bash
+++ src/bin/argc/completions/argc.bash
@@ -3,10 +3,15 @@ _argc_complete_path() {
     local kind="$2"
     if [[ "$kind" == "dir" ]]; then
         COMPREPLY=($(compgen -d -- "${cur}"))
+        compopt -o plusdirs
     else
         COMPREPLY=($(compgen -f -- "${cur}"))
+        local opts
+        if [[ ${#COMPREPLY[@]} -eq 1 ]]; then
+            opts="+o nospace"
+        fi
+        compopt $opts -o plusdirs
     fi
-    compopt -o plusdirs
 }
 
 _argc_complete_impl() {
@@ -16,22 +21,15 @@ _argc_complete_impl() {
     while IFS=$'\n' read -r line; do
         candidates+=( "$line" )
     done < <(argc --argc-compgen bash "$@" 2>/dev/null)
-    local space=0
     local skip=0
     if [[ ${#candidates[@]} -gt 0 ]]; then
         if [[ "${candidates[0]}" == "__argc_value:file" ]]; then
             skip=1
             _argc_complete_path "$cur"
-            if [[ ${#COMPREPLY[@]} -eq 1 ]] && [[ ${#candidates[@]} -eq 1 ]]; then
-                space=1
-            fi
         elif [[ "${candidates[0]}" == "__argc_value:dir" ]]; then
             skip=1
             _argc_complete_path "$cur" dir
         fi
-    fi
-    if [[ $space -eq 1 ]]; then
-        compopt +o nospace
     fi
     COMPREPLY=( "${candidates[@]:$skip}" "${COMPREPLY[@]}" )
 }
