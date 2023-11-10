6b747ee37397f4fc897263b2f0eaa523931c44c6
sigoden <sigoden@gmail.com>
2023-11-10 21:47:32 +0800

    rename ARGC_<SHELL>_COMPLETION to ARGC_COMPLETION_<SHELL>

--- src/bin/argc/completions/mod.rs
+++ src/bin/argc/completions/mod.rs
@@ -18,11 +18,9 @@ const XONSH_SCRIPT: &str = include_str!("argc.xsh");
 
 pub fn generate(shell: Shell, args: &[String]) -> Result<String> {
     let mut cmds = args.to_vec();
-    let shell_completion_name = format!("ARGC_{}_COMPLETION", shell.name().to_uppercase());
-    let shell_completion = std::env::var(&shell_completion_name)
-        .ok()
-        .unwrap_or_default();
-    let append_mode = shell_completion == "1";
+    let completion_shell = format!("ARGC_COMPLETION_{}", shell.name().to_uppercase());
+    let exist_completion = std::env::var(&completion_shell).ok().unwrap_or_default();
+    let append_mode = exist_completion == "1";
     if !append_mode {
         cmds.insert(0, "argc".to_string());
     }
@@ -30,14 +28,14 @@ pub fn generate(shell: Shell, args: &[String]) -> Result<String> {
     let mut cmds_code = String::new();
     match shell {
         Shell::Bash => {
-            share_script = format!("{BASH_SCRIPT}\nexport {shell_completion_name}=1\n");
+            share_script = format!("{BASH_SCRIPT}\nexport {completion_shell}=1\n");
             cmds_code = format!(
                 "complete -F _argc_completer -o nospace -o nosort {}",
                 cmds.join(" ")
             );
         }
         Shell::Elvish => {
-            share_script = format!("{ELVISH_SCRIPT}\nset E:{shell_completion_name} = 1\n");
+            share_script = format!("{ELVISH_SCRIPT}\nset E:{completion_shell} = 1\n");
             cmds_code = cmds
                 .iter()
                 .map(|v| {
@@ -51,7 +49,7 @@ pub fn generate(shell: Shell, args: &[String]) -> Result<String> {
                 .join("\n");
         }
         Shell::Fish => {
-            share_script = format!("{FISH_SCRIPT}\nset -gx {shell_completion_name} 1\n");
+            share_script = format!("{FISH_SCRIPT}\nset -gx {completion_shell} 1\n");
             cmds_code = cmds
                 .iter()
                 .map(|v| format!("complete -x -k -c {v} -a \"(_argc_completer)\""))
@@ -60,7 +58,7 @@ pub fn generate(shell: Shell, args: &[String]) -> Result<String> {
         }
         Shell::Generic => {}
         Shell::Nushell => {
-            share_script = format!("{NUSHELL_SCRIPT}\n$env.{shell_completion_name} = 1\n");
+            share_script = format!("{NUSHELL_SCRIPT}\n$env.{completion_shell} = 1\n");
             if append_mode {
                 cmds_code = format!("$env.ARGC_SCRIPTS = $env.ARGC_SCRIPTS ++ {cmds:?}");
             } else {
@@ -68,11 +66,11 @@ pub fn generate(shell: Shell, args: &[String]) -> Result<String> {
             }
         }
         Shell::Powershell => {
-            share_script = format!("{POWERSHELL_SCRIPT}\n$env:{shell_completion_name} = 1\n");
+            share_script = format!("{POWERSHELL_SCRIPT}\n$env:{completion_shell} = 1\n");
             cmds_code = cmds.iter().map(|v| format!("Register-ArgumentCompleter -Native -ScriptBlock $_argc_completer -CommandName {v}")).collect::<Vec<String>>().join("\n");
         }
         Shell::Xonsh => {
-            share_script = format!("{XONSH_SCRIPT}\n${shell_completion_name} = 1\n");
+            share_script = format!("{XONSH_SCRIPT}\n${completion_shell} = 1\n");
             if append_mode {
                 cmds_code = format!("ARGC_SCRIPTS.extend({cmds:?})");
             } else {
@@ -80,7 +78,7 @@ pub fn generate(shell: Shell, args: &[String]) -> Result<String> {
             }
         }
         Shell::Zsh => {
-            share_script = format!("{ZSH_SCRIPT}\nexport {shell_completion_name}=1\n");
+            share_script = format!("{ZSH_SCRIPT}\nexport {completion_shell}=1\n");
             cmds_code = format!("compdef _argc_completer {}", cmds.join(" "));
         }
     };
