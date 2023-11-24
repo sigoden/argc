8c9c8d47d6f084a995d9da4998246f2a0365d9d6
sigoden <sigoden@gmail.com>
2023-11-24 22:24:24 +0800

    refactor: argc help text

--- src/bin/argc/completions/completion.sh
+++ src/bin/argc/completions/completion.sh
@@ -2,8 +2,8 @@
 
 # @option --argc-eval~ <FILE> <ARGS>                                Use `eval "$(argc --argc-eval "$0" "$@")"`
 # @option --argc-create~ <TASKS>                                    Create a boilerplate argcfile
-# @option --argc-completions~[`_choice_completion`] <SHELL> <CMDS>  Generate completion scripts for bash,elvish,fish,nushell,powershell,xsh,zsh
-# @option --argc-compgen~[`_choice_compgen`] <SHELL> <FILE> <ARGS>  Generate dynamic completion word
+# @option --argc-completions~[`_choice_completion`] <SHELL> <CMDS>  Generate shell completion scripts
+# @option --argc-compgen~[`_choice_compgen`] <SHELL> <FILE> <ARGS>  Dynamically generating completion candidates
 # @option --argc-export <FILE>                                      Export command line definitions as json
 # @option --argc-parallel~ <FILE> <ARGS>                            Execute argc functions in parallel
 # @flag --argc-script-path                                          Print current argcfile path
