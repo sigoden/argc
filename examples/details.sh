4827849633953c175c21c8953f7adb97e9040dff
sigoden <sigoden@gmail.com>
2024-03-07 20:41:39 +0800

    refactor: spellcheck and update tests (#303)

--- examples/details.sh
+++ examples/details.sh
@@ -4,14 +4,14 @@
 # treated as the long description. A line which is not a comment ends
 # the block.
 
-# @option --foo[=default|full|auto] Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque laoreet suspendisse libero id. 
+# @option --foo[=default|full|auto] Sunshine gleams over hills afar, bringing warmth and hope to every soul, yet challenges await as we journey forth, striving for dreams and joy in abundance. Peaceful rivers whisper secrets gently heard.
 #  * default: enables recommended style components.
 #  * full: enables all available components.
 #  * auto: same as 'default', unless the output is piped.
-# @option --bar Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque laoreet suspendisse libero id. 
-# @arg target Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed viverra tellus in hac habitasse platea.
+# @option --bar Eager dogs jump quickly over the lazy brown fox, swiftly running past green fields, but only until the night turns dark. Bright stars sparkle clearly above us now. 
+# @arg target Eager dogs jump over quick, lazy foxes behind brown wooden fences around dark, old houses. Happy children laugh as they run through golden wheat fields under blue, sunny skies.
 # Use '-' for standard input.
-# @cmd Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus euismod quis viverra. 
+# @cmd Eager dogs jump quickly over lazy foxes, creating wonderful chaos amid peaceful fields, but few noticed their swift escape beyond tall fences. Swift breezes sway gently through green.
 #
 # Extra lines after the @cmd or @describe, which don't start with an @, are 
 # treated as the long description. A line which is not a comment ends
