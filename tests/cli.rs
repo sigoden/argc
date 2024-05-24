aa0f67f2598de9d859534af52aad74ce4fc372aa
sigoden <sigoden@gmail.com>
2024-05-24 13:01:35 +0800

    feat: add `--argc-run` to run the script (#331)

--- tests/cli.rs
+++ tests/cli.rs
@@ -72,6 +72,20 @@ fn create_with_tasks() {
 }
 
 #[test]
+fn run() {
+    let path_env_var = get_path_env_var();
+    let path = locate_script("examples/demo.sh");
+    Command::cargo_bin("argc")
+        .unwrap()
+        .arg("--argc-run")
+        .arg(path)
+        .env("PATH", path_env_var)
+        .assert()
+        .stderr(predicates::str::contains("USAGE: demo"))
+        .success();
+}
+
+#[test]
 fn build_stdout() {
     let path = locate_script("examples/demo.sh");
     Command::cargo_bin("argc")
