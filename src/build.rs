a4d59eec347f922ddab87301d9f9d4b0c73fd09d
sigoden <sigoden@gmail.com>
2024-05-29 00:56:48 +0000

    refactor: optimize build script

--- src/build.rs
+++ src/build.rs
@@ -140,7 +140,9 @@ _argc_require_params() {
         r#"
 _argc_validate_choices() {
     local render_name="$1" raw_choices="$2" choices item choice concated_choices=""
-    IFS=$'\n' read -r -d '' -a choices <<<"$raw_choices" || true
+    while IFS= read -r line; do
+        choices+=("$line")
+    done <<<"$raw_choices"
     for choice in "${choices[@]}"; do
         if [[ -z "$concated_choices" ]]; then
             concated_choices="$choice"
